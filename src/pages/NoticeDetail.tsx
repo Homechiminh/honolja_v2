4fce69b18af2afc7c8a47dc33fc54f77a7ec25d2
Homechiminh <rhfemtmxk@gmail.com>
2026-02-03 14:08:30 +0700

    Update NoticeDetail.tsx

--- src/pages/NoticeDetail.tsx
+++ src/pages/NoticeDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { supabase } from '../supabase';
